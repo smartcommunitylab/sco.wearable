23f51e7f923e753f868b44dca19e416923e2ef09
davide-calza <davide.calza20@gmail.com>
2018-04-27 22:55:10 +0200

    Stopwatch on tracking

--- app/src/main/java/it/davidecalza/viaggiaplaygo/tracking_on.java
+++ app/src/main/java/it/davidecalza/viaggiaplaygo/tracking_on.java
@@ -1,14 +1,28 @@
 package it.davidecalza.viaggiaplaygo;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.lang.Math.toIntExact;
+import static java.lang.Thread.sleep;
 
 public class tracking_on extends WearableActivity {
 
+    private long start_time;
+    private TextView timer_txt;
+    private long seconds;
+    int minutes;
+    Handler handler;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +34,33 @@ public class tracking_on extends WearableActivity {
             im.setImageResource(bundle.getInt("tracking_mode"));
         }
 
+        timer_txt = findViewById(R.id.timer);
+        timer_txt.setText("Timer 00:00");
+
         // Enables Always-on
         setAmbientEnabled();
+
+        start_time = System.currentTimeMillis();
+
+        handler = new Handler();
+        handler.postDelayed(new Stopwatch(), 500);
+    }
+    private class Stopwatch implements Runnable {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void run() {
+            seconds = (System.currentTimeMillis() - start_time)/1000;
+            if(seconds >= 60){
+                minutes = toIntExact(seconds/60);
+                seconds = seconds-(minutes*60);
+            }
+            String txt_sec = ""+seconds;
+            String txt_min = ""+minutes;
+            if(seconds<10){ txt_sec = "0"+seconds; }
+            if(minutes<10){ txt_min = "0"+minutes; }
+
+            timer_txt.setText("Timer " + txt_min + ":" + txt_sec);
+            handler.postDelayed(this,500);
+        }
     }
 }
